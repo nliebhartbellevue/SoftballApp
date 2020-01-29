9cf226a0b9ae7da3426f7d01b1d877696c41fc6d
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-01-29 15:18:01 -0600

    Error Handling

--- SoftballApp-SPA/src/app/nav/nav.component.ts
+++ SoftballApp-SPA/src/app/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
         next => {
           console.log('Logged in Successfully!');
         }, error => {
-          console.log('Failed to Login!');
+          console.log(error);
         }
       );
   }
