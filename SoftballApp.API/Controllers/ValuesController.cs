13c2272230288cc71dd0ce9e28a064dd3cec8324
nliebhartbellevue <nliebhart@my365.bellevue.edu>
2020-01-29 10:32:12 -0600

    Frontend Authentication

--- SoftballApp.API/Controllers/ValuesController.cs
+++ SoftballApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace SoftballApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
